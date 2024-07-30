2440602f5b290e9e50dcf09841503fa5ed3a4c86
jeun <jeun@shadow.do>
2024-07-30 09:57:02 +0900

    fix dc/dw abbr

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -2,13 +2,13 @@ if status is-interactive
   function multicd
     echo cd (string repeat -n (math (string length -- $argv[1]) - 1) ../)
   end
+
   set -l doc '~/Documents/'
   set -l dw '~/Downloads/'
-  abbr --add cdc --regex '~/dc' --set-cursor $doc"%"
-  abbr --add cdw --regex '~/dw' --set-cursor $dw"%"
+  abbr --add cdc --position anywhere --regex '~/dc' --set-cursor $doc"%"
+  abbr --add cdw --position anywhere --regex '~/dw' --set-cursor $dw"%"
   abbr --add dc --regex '^dc$' --set-cursor $doc"%"
   abbr --add dw --regex '^dw$' --set-cursor $dw"%"
-
   test -e {$HOME}/.iterm2_shell_integration.fish ; and source {$HOME}/.iterm2_shell_integration.fish
 
   starship init fish | source
