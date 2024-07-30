534164d81dd2eb6bea39d884214fe670c4b4220d
jeun <jeun@shadow.do>
2024-07-30 10:04:06 +0900

    fix dc/dw abbr

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -2,6 +2,7 @@ if status is-interactive
   function multicd
     echo cd (string repeat -n (math (string length -- $argv[1]) - 1) ../)
   end
+  abbr --add dotdot --regex '^\.\.+$' --function multicd
 
   set -l doc '~/Documents/'
   set -l dw '~/Downloads/'
