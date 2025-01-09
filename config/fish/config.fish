c5bd237d8390432677a3d6f8796b893ba4d3c983
jason <junghyun.eun@gmail.com>
2025-01-09 23:02:39 +0900

    changed fish config

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -1,3 +1,20 @@
+# git aliases
+alias ga 'git add'
+alias gb 'git br'
+alias gc 'git commit'
+alias gca 'git commit --amend'
+alias gcl 'git clone'
+alias gco 'git co'
+alias gd 'git diff'
+alias gm 'git merge'
+alias gp 'git push'
+alias gpo 'git push origin HEAD'
+alias gr 'git co -- .'
+alias gs 'git st'
+
+# Others
+set -x MANPAGER "sh -c 'col -bx | bat --color=always --theme=1337 -l man -p'"
+
 if status is-interactive
   function multicd
     echo cd (string repeat -n (math (string length -- $argv[1]) - 1) ../)
@@ -6,12 +23,11 @@ if status is-interactive
 
   set -l doc '~/Documents/'
   set -l dw '~/Downloads/'
-  abbr --add cdc --position anywhere --regex '~/dc' --set-cursor $doc"%"
-  abbr --add cdw --position anywhere --regex '~/dw' --set-cursor $dw"%"
   abbr --add dc --regex '^dc$' --set-cursor $doc"%"
   abbr --add dw --regex '^dw$' --set-cursor $dw"%"
-  test -e {$HOME}/.iterm2_shell_integration.fish ; and source {$HOME}/.iterm2_shell_integration.fish
 
   starship init fish | source
+  test -e {$HOME}/.iterm2_shell_integration.fish ; and source {$HOME}/.iterm2_shell_integration.fish
+
 end
 
