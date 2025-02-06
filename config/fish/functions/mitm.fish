9ca56be74e13feaff76933f6eabf8a6c80fda1a1
jason <junghyun.eun@gmail.com>
2025-02-07 04:39:16 +0900

    remove mitm stuffs

--- config/fish/functions/mitm.fish
+++
@@ -1,9 +0,0 @@
-function mitm
-  set -l ffp $fish_function_path[1]
-  env fish $ffp/proxy_on.fish
-  mitmweb --set tls_version_client_min=SSL3
-  echo "Exiting mitmweb..."
-  echo "Turning off proxy..."
-  env fish $ffp/proxy_off.fish
-  echo "Finished cleaning up."
-end
--- config/fish/functions/proxy_off.fish
+++
@@ -1,8 +0,0 @@
-function proxy_off
-  set -l activeNetwork (route get default | grep interface | awk '{print $2}')
-  set -l activeNetworkName (networksetup -listallhardwareports | grep -B 1 "$activeNetwork" | awk '/Hardware Port/{ print }'|cut -d " " -f3-)
-  networksetup -setwebproxystate "$activeNetworkName" off
-  networksetup -setsecurewebproxystate "$activeNetworkName" off
-  networksetup -setwebproxystate "Wi-Fi" off
-  networksetup -setsecurewebproxystate "Wi-Fi" off
-end
