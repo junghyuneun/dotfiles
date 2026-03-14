33f7a67df876807943ddeb223e5ee9a6c81b3d64
junghyuneun <junghyun.eun@gmail.com>
2026-03-15 00:31:00 +0900

    iterm2 fixes, wiz script updates, etc

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -7,25 +7,23 @@ alias cat 'bat'
 alias ls 'eza'
 
 # Others
-set -x MANPAGER "sh -c 'col -bx | bat --color=always --theme=1337 -l man -p'"
+set -x MANPAGER "sh -c 'col -bx | bat --theme=\"Catppuccin Frappe\" -l man -p'"
 
 if status is-interactive
   function multicd
     echo cd (string repeat -n (math (string length -- $argv[1]) - 1) ../)
   end
 
-  set -l dw '~/Downloads/'
+  set -l dw ~/Downloads/
   abbr --add dw --regex '^dw$' --set-cursor $dw"%"
 
-  oh-my-posh init fish --config $XDG_CONFIG_HOME/oh-my-posh/config.omp.json | source
-  set -l fp "$HOME/.iterm2_shell_integration.fish"
-  test -e $fp; and source $fp
+  oh-my-posh init fish --config $XDG_CONFIG_HOME/oh-my-posh/config.omp.* | source
   clean_fnm
 end
 
 
 # pnpm
-set -gx PNPM_HOME "/Users/jasoneun/.local/share/pnpm"
+set -gx PNPM_HOME "$HOME/.local/share/pnpm"
 if not string match -q -- $PNPM_HOME $PATH
   set -gx PATH "$PNPM_HOME" $PATH
 end
@@ -37,5 +35,5 @@ end
 #   - the correct directories to the PATH
 #   - auto-completion for the opam binary
 # This section can be safely removed at any time if needed.
-test -r '/Users/jasoneun/.opam/opam-init/init.fish' && source '/Users/jasoneun/.opam/opam-init/init.fish' > /dev/null 2> /dev/null; or true
+test -r "$HOME/.opam/opam-init/init.fish" && source "$HOME/.opam/opam-init/init.fish" > /dev/null 2> /dev/null; or true
 # END opam configuration
--- config/fish/functions/wiz.fish
+++ config/fish/functions/wiz.fish
@@ -1,37 +1,67 @@
 function wiz
-  argparse -x on,off u/on d/off c/color=? b/brightness=? s/speed=? -- $argv
+  if test (count $argv) -eq 0
+    echo "Specify at least one action."
+    return
+  end
+  # Possible commands.
+  set -f cmds on off get set
+  set -f action $argv[1]
+  if not contains $action $cmds
+    echo "Use one of 'on', 'off', 'get', 'set'."
+    return
+  end
+  set -e argv[1]
+  if test $action != "set" -a (count $argv) -gt 0
+    echo "No arguments should be used with 'on', 'off' or 'get'."
+    return
+  end
+  argparse c/color= b/brightness= s/speed= -- $argv
   or return
 
+  # Current list of all available modes in Wiz app.
   set -f MODES {"Ocean", "Romance", "Sunset", "Party", "Fireplace", "Cozy", "Forest", "Pastel colors", "Wake-up", "Bedtime", "Warm white", "Daylight", "Cool white", "Night light", "Focus", "Relax", "True colors", "TV time", "Plant growth", "Spring", "Summer", "Fall", "Deep dive", "Jungle", "Mojito", "Club", "Christmas", "Halloween", "Candlelight", "Golden white", "Pulse", "Steampunk", "Diwali", "White", "Alarm", "Rhythm"}
+
+  # Set the IP of my WIZ bulb in DHCP settings.
+  # Also set other repetitive parameters.
   set -f WIZ 192.168.0.2
-  if set -q _flag_off || set -q _flag_on
-    set -f METHOD setState
-    set -f PARAMS "\"state\":$(if set -q _flag_off; echo false; else; echo true; end)"
-  else
-    set -f METHOD setPilot
-    if set -q _flag_c
-      set -f matched 0
-      for i in (seq (count $MODES))
-        if string match -i "*$_flag_c*" $MODES[$i] >/dev/null
-          set matched 1
-          break
+  set -f ON_OR_OFF on off
+  set -f P_ID \"id\":1
+  set -f P_METHOD \"method\":\"$(if contains $action $ON_OR_OFF; echo -n "setState";else; echo -n $action"Pilot"; end)\"
+  set -f INNER_PARAMS \"state\":$(if test $action = "on"; echo -n true; else; echo -n false; end)
+
+  # Set parameters in accordance with the actions.
+  switch $action
+    # get action doesn't take any parameters.
+    case get
+      set -f P_ID
+      set -f INNER_PARAMS
+    # set action can take "speed", "brightness" and "color (mode)".
+    case set
+      if set -q _flag_c
+        set -f matched 0
+        for i in (seq (count $MODES))
+          if string match -i "*$_flag_c*" $MODES[$i] >/dev/null
+            set matched 1
+            break
+          end
         end
+        if test $matched -eq 0
+          set i 13
+        end
+        set -f COLOR_PARAM "\"sceneId\":$i"
       end
-      if test $matched -eq 0
-        set i 13
+      if set -q _flag_b
+        set -f DIMMING_PARAM "\"dimming\":$_flag_b"
       end
-      set -f COLOR_PARAM "\"sceneId\":$i"
-    end
-    if set -q _flag_b
-      set -f DIMMING_PARAM "\"dimming\":$_flag_b"
-    end
-    if set -q _flag_s
-      set -f SPEED_PARAM "\"speed\":$_flag_s"
-    end
-    set -f PARAMS $(string join , $COLOR_PARAM $DIMMING_PARAM $SPEED_PARAM)
+      if set -q _flag_s
+        set -f SPEED_PARAM "\"speed\":$_flag_s"
+      end
+      set -f INNER_PARAMS $(string join --no-empty , $COLOR_PARAM $DIMMING_PARAM $SPEED_PARAM)
   end
-  set -f PAYLOAD "{\"id\":1,\"method\":\"$METHOD\",\"params\":{$PARAMS}}"
+
+  set -f PARAMS "\"params\":{$INNER_PARAMS}"
+  set -f PAYLOAD "{$(string join --no-empty , $P_ID $P_METHOD $PARAMS)}"
   echo $PAYLOAD
-  echo -n $PAYLOAD | nc -u -w 1 $WIZ 38899 >/dev/null
+  echo -n $PAYLOAD | nc -u -w 1 $WIZ 38899
 end
 
